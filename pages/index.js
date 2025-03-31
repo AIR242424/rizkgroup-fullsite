22b7a6df644b8331b1540489eee513b98c76fe3c
AIR242424 <rizkalain@hotmail.com>
2025-03-31 18:13:26 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-      <h1>Welcome to Rizk Group</h1>
+      <h1>Welcome to Rizk Group AI</h1>
     </Layout>
   );
 }
